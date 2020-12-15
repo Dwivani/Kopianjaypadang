d740f52a6dfdfb9294105c733cb447cb4fb0850b
Dwi vani <76057358+Dwivani@users.noreply.github.com>
2020-12-16 00:18:14 +0700

    Add files via upload

---
+++ data/titik_1.js
@@ -0,0 +1,8 @@
+var json_titik_1 = {
+"type": "FeatureCollection",
+"name": "titik_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 100.361299512206301, -0.934249540000932 ] } }
+]
+}
